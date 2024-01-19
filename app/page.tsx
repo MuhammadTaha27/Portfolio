381fd844bc23ab5ee28feb71c81918b36834ed64
MuhammadTaha27 <76904648+MuhammadTaha27@users.noreply.github.com>
2024-01-19 15:16:44 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Skills from './components/Skills'
 import Services from './components/Services'
 import Footer from './components/Footer'
 import Reviews from './components/Reviews'
-import Abc from './components/Abc'
 import Projects from './components/Projects'
 
 
